ec6f8875d85edc2d484af1eab2d171a9390dd3ce
rhagan <rhagan610@gmail.com>
2025-03-19 16:38:55 +0000

    Added doughnut charts to home page

--- Home.php
+++ Home.php
@@ -1,3 +1,35 @@
+<?php 
+
+    $db = new SQLite3('ElancoDB.db');
+
+    //get the time from when the user opens the website
+    $currentTime = date("G");
+
+    //account for databases time format (starting at 0)
+    $currentTime -= 1;
+        
+    //run a select statement to get the water intake of the dog throughout the day 
+    $query = $db->prepare('SELECT AVG(Weight) AS avgWeight, SUM(Water_Intake) AS totalIntake, SUM(Calorie_Burn) AS totalBurnt FROM Activity WHERE Hour <= :currentTime AND DogID = "CANINE001" AND Date = "01-01-2021" ');
+    $query->bindValue(":currentTime", $currentTime, SQLITE3_INTEGER);
+    $result = $query->execute();
+
+    $row = $result->fetchArray(SQLITE3_ASSOC);
+
+    $totalIntake = round($row['totalIntake']);
+    $weight = $row['avgWeight'];
+    $totalBurnt = round($row['totalBurnt']);
+
+    //calculate the ammount of water the dog needs 
+    $totalMl = round($weight * 60);
+
+    //check if the goal has been hit 
+    if ($totalIntake > $totalMl){
+        $intakeLeft = 0;
+    } else{
+        //if the goal hasn't been hit then calculate the ammount left
+        $intakeLeft = $totalMl - $totalIntake;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,6 +37,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-doughnutlabel"></script>
+    <script src="chart.js"></script>
     <style>
         form.notes {
             float: left;
@@ -32,6 +67,22 @@
         li.dogOption {
             margin-top: 14px;
         }
+
+        .warning{
+            display: flex;
+            top: 20px;
+            right: 20px;
+            font-size: 10px;
+            font-style: italic;
+        }
+        .charts {
+            margin-top: 50px;
+            width: 500px;
+            height: 500px;
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+        }
     </style>
 </head>
 
@@ -51,8 +102,33 @@
             <br><br>
             <label>Temperature: 28.5 C</label>
         </form>
-        <img class="Placeholder" src="PlaceholderAnalysis.png" alt="Placeholder">
+    </div>
 
+    <div class="warning">
+        <h1>*Please note all goals are general and may not be specific to your dog</h1>
+    </div>
+    
+    <script>
+    window.onload = function() {
+        loadDoughChart('doughChart', <?php echo json_encode($totalIntake)?>, <?php echo json_encode($intakeLeft)?>, 'Water Intake(ml)', <?php echo json_encode($totalMl)?>);
+        loadDoughChart('burnt', <?php echo json_encode($totalBurnt)?>, 100, 'Calories Burnt', 600);
+        loadDoughChart('calorieIntake', 150, 250, 'Calories needed', 550);
+        loadDoughChart('steps', 3000, 2000, 'Step Goal', 5000)
+    }
+    </script>
+    <div class="charts">
+        <div class="chart">
+            <canvas id="doughChart"></canvas>
+        </div>
+        <div class="chart">
+            <canvas id="burnt"></canvas>
+        </div>
+        <div class="chart">
+            <canvas id="calorieIntake"></canvas>
+        </div>  
+        <div class="chart">
+            <canvas id="steps"></canvas>
+        </div>  
     </div>
 </body>
 
--- doughnutExample.php
+++ doughnutExample.php
@@ -9,7 +9,7 @@
     $currentTime -= 1;
         
     //run a select statement to get the water intake of the dog throughout the day 
-    $query = $db->prepare('SELECT AVG(Weight) AS avgWeight, SUM(Water_Intake) AS totalIntake FROM Activity WHERE Hour <= :currentTime AND DogID = "CANINE001" AND Date = "01-01-2021" ');
+    $query = $db->prepare('SELECT AVG(Weight) AS avgWeight, SUM(Water_Intake) AS totalIntake, SUM(Calorie_Burn) AS totalBurnt FROM Activity WHERE Hour <= :currentTime AND DogID = "CANINE001" AND Date = "01-01-2021" ');
     $query->bindValue(":currentTime", $currentTime, SQLITE3_INTEGER);
     $result = $query->execute();
 
@@ -17,6 +17,7 @@
 
     $totalIntake = round($row['totalIntake']);
     $weight = $row['avgWeight'];
+    $totalBurnt = round($row['totalBurnt']);
 
     //calculate the ammount of water the dog needs 
     $totalMl = round($weight * 60);
@@ -39,12 +40,43 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-doughnutlabel"></script>
     <script src="chart.js"></script>
-</head>
-<body onload="loadDoughChart('doughChart', <?php echo json_encode($totalIntake)?>, <?php echo json_encode($intakeLeft)?>, 'Water Intake(ml)', <?php echo json_encode($totalMl)?>)">
+    <link rel="stylesheet" href="summary.css"> 
+    <style>
+        .charts {
+            margin-top: 50px;
+            width: 500px;
+            height: 500px;
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+        }
+    </style>
 
-<div class="chart">
-    <canvas id="doughChart" style="width:100%;max-width:700px;"></canvas>
+</head>
+<body>
+
+<script>
+window.onload = function() {
+    loadDoughChart('doughChart', <?php echo json_encode($totalIntake)?>, <?php echo json_encode($intakeLeft)?>, 'Water Intake(ml)', <?php echo json_encode($totalMl)?>);
+    loadDoughChart('burnt', <?php echo json_encode($totalBurnt)?>, 100, 'Calories Burnt', 600);
+    loadDoughChart('calorieIntake', 150, 250, 'Calories needed', 550);
+    loadDoughChart('steps', 3000, 2000, 'Step Goal', 5000)
+}
+</script>
+<div class="charts">
+    <div class="chart">
+        <canvas id="doughChart"></canvas>
+    </div>
+    <div class="chart">
+        <canvas id="burnt"></canvas>
+    </div>
+    <div class="chart">
+        <canvas id="calorieIntake"></canvas>
+    </div>  
+    <div class="chart">
+        <canvas id="steps"></canvas>
+    </div>  
 </div>
-    
+
 </body>
 </html>
