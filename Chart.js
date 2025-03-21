74e051fc5635419641326cc185f4f5c9f2bbf261
Myles Fairburn <mylesfairburn257@gmail.com>
2025-03-21 17:57:52 +0000

    Created bar chart for steps.

--- Chart.js
+++ Chart.js
@@ -87,14 +87,13 @@ function loadLineGraph(canvasId, dataset, outputDataset, graphLabel, yLabel, xLa
     }
 }
 
-function loadBarChart(canvasId, dataset, outputDataset, graphLabel, yLabel, xLabel, extraOutputLabel){
+function loadBarChart(canvasId, dataset, outputDataset, xValues, graphLabel, yLabel, xLabel, extraOutputLabel){
 
     if (!dataset.length || !outputDataset.length) {
         alert("Some data is missing for the selected date.");
         return;
     } // error handling if theres some data missing
     else{
-        const xValues = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 
         const ctx = document.getElementById(canvasId);
 
@@ -222,7 +221,7 @@ function loadBehaviorPieChart(canvasId, behaviorDataset, graphLabel) {
                 }
             }
         });
-    }}
+}}
 
 function loadDoughChart(canvasId, progress, valueLeft, title, total){
 
--- Steps.php
+++ Steps.php
@@ -34,9 +34,8 @@
     }
 
     $db = new SQLite3('ElancoDB.db');
-    $breathingData = [];
+    $activityLevelData = [];
     $behaviourData = [];
-
 
     // Get the row number of the date the user enters
     if ($newDate != null) {
@@ -64,25 +63,71 @@
         }
         echo "Selected Date: " . $newDate ."<br>";
 
-        // Fetch breathing rates for the given date
-        $query = $db->prepare('SELECT Breathing_Rate FROM Activity WHERE Date = :newDate AND Hour >= 0 AND Hour <= 23 AND DogID = "CANINE001"');
+        // Fetch steps for the given date
+        $query = $db->prepare('SELECT Activity_Level FROM Activity WHERE Date = :newDate AND Hour >= 0 AND Hour <= 23 AND DogID = "CANINE001"');
         $query->bindValue(':newDate', $newDate, SQLITE3_TEXT);
         $result = $query->execute();
 
         // Check if the query executed successfully
         if (!$result) {
-            echo "Error executing query for breathing rates.";
+            echo "Error executing query for steps.";
             exit();
         }
 
         if ($result->numColumns() == 0) {
-            echo "No breathing rate data found for the selected date: " . $newDate;
+            echo "No steps data found for the selected date: " . $newDate;
             exit();
         }
 
-        // Populate $breathingData array with breathing rates
-        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-            $breathingData[] = $row['Breathing_Rate'];
+        // Populate $activityLevelData array with breathing rates
+        while($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $activityLevelData[] = $row['Activity_Level'];
+        }
+
+        $prevEmpty = 0; // used to find if there is a range of hours where the dog does not complete any steps
+        $count = 0; // indexing array
+        $dataToDelete = []; // array used to store which indexes of the $activityLevelData array should be deleted
+        $hours = [];
+
+        while ($count < count($activityLevelData)) { // loop though the $activityLevelData array
+            if ($activityLevelData[$count] == 0) { // if the dog completes 0 steps that hour
+                $prevEmpty++; 
+
+                if ($count == 23) { // error handling if the dog completes 0 steps in the final hour of the day
+                    if ($prevEmpty == 1) {
+                        $hours[] = $count;
+                    } 
+                    else {
+                        $startNum = $count - $prevEmpty;
+                        $hours[] = $startNum . " - " . ($count); // creates a range of hours where 0 steps are completed
+                    }
+                } 
+                else {
+                    if ($count + 1 < count($activityLevelData) && $activityLevelData[$count + 1] == 0) {
+                        $dataToDelete[] = $count;
+                    } // only adds to the $dataToDelete array if the next hour is also 0
+                }
+            } else {
+                if ($prevEmpty > 2) {
+                    $startNum = $count - $prevEmpty;
+                    $hours[] = $startNum . " - " . ($count - 1); // creates a range of hours where 0 steps are completed
+                } 
+                else if ($prevEmpty == 1 || $prevEmpty == 2) {
+                    for ($i = $count - $prevEmpty; $i < $count; $i++) {
+                        $hours[] = $i;
+                    } // retains the hour when only 1 or 2 hours have 0 steps
+                }
+
+                $hours[] = $count;
+                $prevEmpty = 0;
+            }
+
+            $count++;
+        }
+
+        // delete data in reverse order to avoid index shifting - have to do this in a diffrent loop otherwise it would break the first while loop
+        foreach (array_reverse($dataToDelete) as $index) {
+            array_splice($activityLevelData, $index, 1);
         }
 
         // Fetch behaviour patterns for the given date
@@ -113,6 +158,7 @@
         while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
             $behaviourData[] = $row['Behaviour_Pattern'];
         }
+
     } else {
         echo "Invalid or missing date input.";
         exit();
@@ -123,21 +169,22 @@
     </div>
 
     <script> 
-            window.onload = function() {
-                loadBarChart(
-                    'barChart', 
-                    <?php echo json_encode($breathingData); ?>, // dataset to be displayed as the line
-                    <?php echo json_encode($behaviourData); ?>, // dataset to be displayed when hoverin over a point on the graph
-                    'Breathing Rate', // line label
-                    'Breaths / Minute', // y axes label
-                    'Hour', // x axes label
-                    'Activity: ' // label for the dataset when hovering over a point on the graph
-                );
-            };
+        window.onload = function() {
+            loadBarChart(
+                'barChart', // chart ID
+                <?php echo json_encode($activityLevelData); ?>, // dataset to be displayed as the line
+                <?php echo json_encode($behaviourData); ?>, // dataset to be displayed when hoverin over a point on the graph
+                <?php echo json_encode($hours); ?>,
+                'Activity Level', // line label
+                'Steps', // y axes label
+                'Hour', // x axes label
+                'Activity: ' // label for the dataset when hovering over a point on the graph
+            );
+        };
     </script>
 
     <div class="chart">
-        <canvas id="lineGraph" style="width:100%;max-width:700px;"></canvas>
+        <canvas id="barChart" style="width:100%;max-width:700px;"></canvas>
     </div>
 </body>
 
